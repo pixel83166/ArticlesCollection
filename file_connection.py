ce005260a75bdc5ab0105f82a7a16de436bcabcd
pixel83166 <aherovon@gmail.com>
2024-04-01 13:26:05 +0300

    Initial commit

---
+++ file_connection.py
@@ -0,0 +1,25 @@
+import json
+
+
+def get_articles():
+    with open("articles.json", "r", encoding="utf-8") as art:
+        f = json.load(art)
+    return f
+
+
+def save_article(name, text):
+    save_file = get_articles()
+
+    save_file[name] = text
+
+    with open("articles.json", "w", encoding="utf-8") as arti:
+        json.dump(save_file, arti, ensure_ascii=False)
+
+
+def delete_article(name):
+    delete_file = get_articles()
+
+    del delete_file[name]
+
+    with open("articles.json", "w", encoding="UTF-8") as soh_file:
+        json.dump(delete_file, soh_file, ensure_ascii=False)
